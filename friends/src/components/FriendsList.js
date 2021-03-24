d4860717addc3858ae7433135cb7876ecdd6ec91
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-23 17:14:12 -0700

    Friends List refactor

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-// import PrivateRoute from "./PrivateRoute";
-// import Friend from "./Friend";
 
 const initialState = {
     friends: []
@@ -23,15 +21,20 @@ const FriendsList = () => {
                 console.log(err)
             })
     }, []);
+
+    if (friends.length === 0)
+        return <h3>Loading Friends...</h3>
+
     return (
     <>
     <div>
         <h2>Friends List</h2>
         <Link to="/new-friend">Add New Friend</Link>
-        {friends.length > 0 && friends.map(friend => {
-
-            return <Link to={`/friends-list/${friend.id}`}><h3>{friend.name}</h3></Link>
-        })}
+        {friends.length !== 0 
+        ? friends.map(friend => <Link to={`/friends-list/${friend.id}`}><h3>{friend.name}</h3></Link>
+        ) : (
+            <h3>Loading Friends...</h3>
+        )}
     </div>
     </>
     );
